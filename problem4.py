1bcae66b8c6abfe645c0055dda7720cb05f78123
mihailstt <mihailst977@gmail.com>
2021-11-08 17:53:26 +0200

    classromproblem4.py

---
+++ problem4.py
@@ -0,0 +1,23 @@
+#probelm4
+a = 4
+if a == 0:
+    print("Zero")
+elif a == 1:
+    print("one")
+elif a == 2:
+    print("two")
+elif a == 3:
+    print("three")
+elif a == 4:
+    print("four")
+elif a == 5:
+    print("five")
+elif a == 6:
+    print("six")
+elif a == 7:
+    print("seven")
+elif a == 8:
+    print("eight")
+elif a == 9:
+    print("nine")
+
